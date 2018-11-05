8ba2bf3938c5e0ab56ced7ad3ae71f147da87324
Shuya Motouchi <syu.m.5151@gmail.com>
2018-11-05 15:44:31 +0900

    Update login.go

--- cmd/login.go
+++ cmd/login.go
@@ -1,21 +1,26 @@
 package cmd
 
 import (
-	"fmt"
-	"github.com/spf13/cobra"
-	_ "github.com/spf13/viper"
-	"os/exec"
+        "fmt"
+        "github.com/spf13/cobra"
+        _ "github.com/spf13/viper"
+        "os/exec"
 )
 
 func init() {
-	RootCmd.AddCommand(loginCmd)
+        RootCmd.AddCommand(loginCmd)
 }
 
 var loginCmd = &cobra.Command{
-	Use:   "login",
-	Short: "Calculator of addition.",
-	Long:  "Calculator to perform the addition.",
-	Run: func(cmd *cobra.Command, args []string) {
-		out, err := exec.Command("ls", "-la").Output()
-	},
+        Use:   "login",
+        Short: "Calculator of addition.",
+        Long:  "Calculator to perform the addition.",
+        Run: func(cmd *cobra.Command, args []string) {
+                cmdstr := "w;uptime;ps auxf;ip a;df -TH"
+                out, err := exec.Command("sh", "-c", cmdstr).Output()
+                if err != nil {
+                        fmt.Printf("%s", err)
+                }
+                fmt.Printf("login: %s", out)
+        },
 }
